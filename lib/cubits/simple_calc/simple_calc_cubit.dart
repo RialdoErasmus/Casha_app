3433b6dc10483d51786eb031866365099992ca7a
Rialdo Erasmus <576655@student.belgiumcampus.ac.za>
2022-08-15 10:32:43 +0200

    Changes made to shorten code

--- lib/cubits/simple_calc/simple_calc_cubit.dart
+++ lib/cubits/simple_calc/simple_calc_cubit.dart
@@ -27,121 +27,21 @@ class SimpleCalcCubit extends Cubit<SimpleCalcState> {
 
     // TODO - Calculate your breakdown here, put the results in a map, with the validDenominations as the key, and the result as the value
 
-    if (tender >= cost) {
-      //check if the amount paid is more than the cost of the product. If the amount paid is larger than the cost the application should display the amount of change the person should recieve
-      while (totalChange >= 200) {
-        //Check how many R200 notes the person should recieve as change
-        breakdown.update('200', (value) => ++value);
-        totalChange -= 200;
-      }
-
-      while (totalChange >= 50) {
-        //Check how many R50 notes the person should recieve as change
-        breakdown.update('50', (value) => ++value);
-        totalChange -= 50;
-      }
-
-      while (totalChange >= 20) {
-        //Check how many R20 notes the person should recieve as change
-        breakdown.update('20', (value) => ++value);
-        totalChange -= 20;
-      }
-
-      while (totalChange >= 10) {
-        //Check how many R10 notes the person should recieve as change
-        breakdown.update('10', (value) => ++value);
-        totalChange -= 10;
-      }
-
-      while (totalChange >= 5) {
-        //Check how many R5 coins the person should recieve as change
-        breakdown.update('5', (value) => ++value);
-        totalChange -= 5;
-      }
-
-      while (totalChange >= 2) {
-        //Check how many R2 coins the person should recieve as change
-        breakdown.update('2', (value) => ++value);
-        totalChange -= 2;
-      }
-
-      while (totalChange >= 0.5) {
-        //Check how many 50c coins the person should recieve as change
-        breakdown.update('0.5', (value) => ++value);
-        totalChange -= 0.5;
-      }
-
-      while (totalChange >= 0.2) {
-        //Check how many 20c coins the person should recieve as change
-        breakdown.update('0.2', (value) => ++value);
-        totalChange -= 0.2;
-      }
-
-      while (totalChange >= 1) {
-        //Check how many R1 coins the person should recieve as change
-        breakdown.update('1', (value) => ++value);
-        totalChange -= 1;
-      }
-    } else if (cost > tender) {
-      //Check if the person paying for the product gave enough money. If they did not give enough money the application will show how much they still owe markek by a negative
-      while (totalChange <= -200) {
-        //Check how many R200 notes the person still needs to pay
-        breakdown.update('200', (value) => --value);
-        totalChange += 200;
-      }
-
-      while (totalChange <= -50) {
-        //Check how many R50 notes the person still needs to pay
-        breakdown.update('50', (value) => --value);
-        totalChange += 50;
-      }
-
-      while (totalChange <= -20) {
-        //Check how many R20 notes the person still needs to pay
-        breakdown.update('20', (value) => --value);
-        totalChange += 20;
-      }
-
-      while (totalChange <= -10) {
-        //Check how many R10 notes the person still needs to pay
-        breakdown.update('10', (value) => --value);
-        totalChange += 10;
-      }
-
-      while (totalChange <= -5) {
-        //Check how many R5 coins the person still needs to pay
-        breakdown.update('5', (value) => --value);
-        totalChange += 5;
-      }
-
-      while (totalChange <= -2) {
-        //Check how many R2 coins the person still needs to pay
-        breakdown.update('2', (value) => --value);
-        totalChange += 2;
-      }
-
-      while (totalChange <= -1) {
-        //Check how many R1 coins the person still needs to pay
-        breakdown.update('1', (value) => --value);
-        totalChange += 1;
-      }
-
-      while (totalChange <= -0.5) {
-        //Check how many 50c coins the person still needs to pay
-        breakdown.update('0.5', (value) => --value);
-        totalChange += 0.5;
-      }
-
-      while (totalChange <= -0.2) {
-        //Check how many 20c coins the person still needs to pay
-        breakdown.update('0.2', (value) => --value);
-        totalChange += 0.2;
-      }
+    if (totalChange > 0) {
+      validDenominations.forEach((i) {
+        var val = (totalChange / i).floor();
+        totalChange = totalChange % i;
+        breakdown['${i}'] = val;
+      });
+    } else {
+      print('Not enough funds.');
     }
+
     breakdown.removeWhere((key, value) =>
-        value ==
+        -value ==
         0); //Remove all the items in the map with a key value equal to 0 so only the notes/coins that needs to be given/recieved are displayed by the app
     totalChange = tender - cost;
+
     emit(SimpleCalcCalculated(breakdown, totalChange));
   }
 
